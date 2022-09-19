c72130d207eb0fcedda2da1d3ed3f457e32a9fee
Chew <chew@chew.pw>
2022-09-18 23:07:19 -0500

    code cleaneroni

--- src/main/java/org/jasonkaranik/boatfly/BoatNerfer.java
+++ src/main/java/org/jasonkaranik/boatfly/BoatNerfer.java
@@ -6,8 +6,6 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerKickEvent;
-import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
@@ -16,6 +14,9 @@ import org.spigotmc.event.entity.EntityDismountEvent;
 import java.util.List;
 
 public class BoatNerfer implements Listener {
+    @SuppressWarnings("FieldCanBeLocal")
+    private final int HEIGHT_DIFF = 1;
+
     @EventHandler
     public void onVehicleMove(VehicleMoveEvent event) {
         Entity vehicle = event.getVehicle();
@@ -25,36 +26,26 @@ public class BoatNerfer implements Listener {
             return;
         }
 
-        if (vehicle.getPassengers().isEmpty() || vehicle.getLocation().getBlock().isLiquid()) {
-            vehicle.setGravity(true);
-        } else {
-            vehicle.setGravity(false);
-        }
+        vehicle.setGravity(vehicle.getPassengers().isEmpty() || vehicle.getLocation().getBlock().isLiquid());
 
         // Get the old vector
         Vector oldVector = event.getVehicle().getVelocity();
-        // Print this
-//        System.out.println("Old vector: " + oldVector);
-
-        double oldY = 0;
 
         List<Entity> entities = event.getVehicle().getPassengers();
         // Find if there is a player in the boat
         for (Entity entity : entities) {
             if (entity instanceof Player) {
                 Player player = (Player) entity;
-                ItemStack mainHand = player.getInventory().getItemInMainHand();
-                ItemStack offHand = player.getInventory().getItemInOffHand();
-                boolean up = (mainHand != null && mainHand.getType() == Material.STICK) || (offHand != null && offHand.getType() == Material.STICK);
-                boolean down = (mainHand != null && mainHand.getType() == Material.LEVER) || (offHand != null && offHand.getType() == Material.LEVER);
+                boolean up = isHolding(player, Material.STICK);
+                boolean down = isHolding(player, Material.LEVER);
 
                 if (up) {
                     player.sendActionBar("§a§lAscending");
-                    event.getVehicle().setVelocity(new Vector(oldVector.getX(), oldVector.getY() + 1, oldVector.getZ()));
+                    event.getVehicle().setVelocity(new Vector(oldVector.getX(), oldVector.getY() + HEIGHT_DIFF, oldVector.getZ()));
                 } else if (down){
                     player.sendActionBar("§c§lDescending");
-                    event.getVehicle().setVelocity(new Vector(oldVector.getX(), oldVector.getY() - 1, oldVector.getZ()));
-                } else {
+                    event.getVehicle().setVelocity(new Vector(oldVector.getX(), oldVector.getY() - HEIGHT_DIFF, oldVector.getZ()));
+                } else if (!vehicle.getLocation().getBlock().isLiquid()) {
                     player.sendActionBar("§e§lCoasting");
                 }
             }
@@ -62,21 +53,21 @@ public class BoatNerfer implements Listener {
     }
 
     @EventHandler
-    public void onPlayerKick(PlayerKickEvent event) {
-        // if the player is kicked for flying, and they are in a boat then cancel the event
-        if (event.getReason().equals("Flying is not enabled on this server") && event.getPlayer().getVehicle() != null) {
-            //
-        }
-    }
-
-    @EventHandler
     public void onEntityDismount(EntityDismountEvent event) {
         if (event.getDismounted().getType() == EntityType.BOAT) {
             event.getDismounted().setGravity(true);
-            if (event.getEntity() instanceof Player) {
-                Player player = (Player) event.getEntity();
-                player.sendActionBar("§a§lBoatFly §7- §c§lDisabled");
-            }
         }
+    }
+
+    /**
+     * Determines if the player is holding an item in their hand(s)
+     * @param player The player to check
+     * @param material The material to check for
+     * @return True if the player is holding the item, false otherwise
+     */
+    private boolean isHolding(Player player, Material material) {
+        ItemStack mainHand = player.getInventory().getItemInMainHand();
+        ItemStack offHand = player.getInventory().getItemInOffHand();
+        return (mainHand != null && mainHand.getType() == material) || (offHand != null && offHand.getType() == material);
     }
 }
